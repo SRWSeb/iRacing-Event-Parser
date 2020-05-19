395fc74c7684e96c896f08b356d220940df7a723
SRWSeb <65510662+SRWSeb@users.noreply.github.com>
2020-05-19 19:58:55 +0200

    Parse the date from the CSV to a format that mysql will accept.

--- test.php
+++ test.php
@@ -1,24 +1,57 @@
-<html>
-<body>
-
-<!--<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="fname">
-  <input type="submit">
-</form>
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST['fname'];
-  if (empty($name)) {
-    echo "Name is empty";
-  } else {
-    echo $name;
-  }
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "srw_champ";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if(!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
-?>-->
+echo "Connected successfully!";
 
-<?php
-  //read .csv from file
+  function enterResult($conn) {
+    //read .csv from file
+    $result = file('data.csv');
+
+    //Remove unnecessary Newlines
+    $result = array_diff($result,["\n"]);
+
+    //Remove first line, don't need that
+    array_shift($result);
+    //Extract Event Infos into
+    $eventInfo = str_getcsv(array_shift($result));
+    //Remove third line, not needed
+    array_shift($result);
+    //Extract League Info into array
+    $leagueInfo = str_getcsv(array_shift($result));
+    //Remove fifth line, not needed
+    array_shift($result);
+
+    $datetime = chop($eventInfo[0],"GMT");
+    $datetime = date_parse_from_format("Y.m.d h:i a",$datetime);
+
+    $finaldatetime ="'" . $datetime['year'] . "-" . $datetime['month'] . "-" . $datetime['day'] . " " . $datetime['hour'] . ":" . $datetime['minute'] . ":00'";
+    echo $finaldatetime;
+
+
+    $datetime2 = "'2020-05-15 14:45:00'";
+
+    $sql_eventInfo = "INSERT INTO events (time_and_day, lone_event, league_id, season_id, teamevent, multiclass) VALUES ($finaldatetime, FALSE, " . $leagueInfo[1] . ", " . $leagueInfo[3] . ", FALSE, FALSE);";
+
+    if($conn->query($sql_eventInfo) === TRUE) {
+      $last_id = $conn->insert_id;
+      echo "<br>Entry successful! Insert ID is: " . $last_id;
+    } else {
+      echo "Error: " . $sql_eventInfo . "<br>" . $conn->error;
+    }
+}
+
+enterResult($conn);
+
+$conn->close();
+
+  /*//read .csv from file
   $result = file('data.csv');
 
   //Remove unnecessary Newlines
@@ -51,9 +84,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       echo "<li>" . $eventStandings[$row][$col] . "</li>";
     }
     echo "</ul>";
-  }
+  }*/
 
  ?>
+
+<html>
+<body>
+
+<form>
+  <button formaction="<?php ?>"
+</form>
+
+
 
 </body>
 </html>
