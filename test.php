aec10cf99c5ed24bb102c80bf3c3a970fd693db6
SRWSeb <65510662+SRWSeb@users.noreply.github.com>
2020-05-19 20:05:31 +0200

    Put the Date Parser into it's own function

--- test.php
+++ test.php
@@ -10,48 +10,17 @@ if(!$conn) {
 }
 echo "Connected successfully!";
 
-  function enterResult($conn) {
-    //read .csv from file
-    $result = file('data.csv');
-
-    //Remove unnecessary Newlines
-    $result = array_diff($result,["\n"]);
-
-    //Remove first line, don't need that
-    array_shift($result);
-    //Extract Event Infos into
-    $eventInfo = str_getcsv(array_shift($result));
-    //Remove third line, not needed
-    array_shift($result);
-    //Extract League Info into array
-    $leagueInfo = str_getcsv(array_shift($result));
-    //Remove fifth line, not needed
-    array_shift($result);
-
-    $datetime = chop($eventInfo[0],"GMT");
-    $datetime = date_parse_from_format("Y.m.d h:i a",$datetime);
-
-    $finaldatetime ="'" . $datetime['year'] . "-" . $datetime['month'] . "-" . $datetime['day'] . " " . $datetime['hour'] . ":" . $datetime['minute'] . ":00'";
-    echo $finaldatetime;
-
-
-    $datetime2 = "'2020-05-15 14:45:00'";
-
-    $sql_eventInfo = "INSERT INTO events (time_and_day, lone_event, league_id, season_id, teamevent, multiclass) VALUES ($finaldatetime, FALSE, " . $leagueInfo[1] . ", " . $leagueInfo[3] . ", FALSE, FALSE);";
-
-    if($conn->query($sql_eventInfo) === TRUE) {
-      $last_id = $conn->insert_id;
-      echo "<br>Entry successful! Insert ID is: " . $last_id;
-    } else {
-      echo "Error: " . $sql_eventInfo . "<br>" . $conn->error;
-    }
+function parseDate($datetime) {
+  //Get rid of the GMT
+  $datetime = chop($datetime,"GMT");
+  //Parse Date into usable array
+  $datetime = date_parse_from_format("Y.m.d h:i a",$datetime);
+  //Build string for MySQL and return it
+  return "'" . $datetime['year'] . "-" . $datetime['month'] . "-" . $datetime['day'] . " " . $datetime['hour'] . ":" . $datetime['minute'] . ":00'";
 }
 
-enterResult($conn);
-
-$conn->close();
-
-  /*//read .csv from file
+function enterResult($conn) {
+  //read .csv from file
   $result = file('data.csv');
 
   //Remove unnecessary Newlines
@@ -68,34 +37,39 @@ $conn->close();
   //Remove fifth line, not needed
   array_shift($result);
 
-  echo "$eventInfo[1] <br>";
-  echo "$leagueInfo[0] <br>";
+  $datetime = parseDate($eventInfo[0]);
+  /*
+  $datetime = chop($eventInfo[0],"GMT");
+  $datetime = date_parse_from_format("Y.m.d h:i a",$datetime);
 
-  $eventStandings = [];
+  $finaldatetime ="'" . $datetime['year'] . "-" . $datetime['month'] . "-" . $datetime['day'] . " " . $datetime['hour'] . ":" . $datetime['minute'] . ":00'";
+  echo $finaldatetime;*/
 
-  foreach ($result as $key => $value) {
-    $eventStandings[$key] = str_getcsv(array_shift($result));
+  $sql_eventInfo = "INSERT INTO events (time_and_day, lone_event, league_id, season_id, teamevent, multiclass) VALUES ($datetime, FALSE, " . $leagueInfo[1] . ", " . $leagueInfo[3] . ", FALSE, FALSE);";
+
+  if($conn->query($sql_eventInfo) === TRUE) {
+    $last_id = $conn->insert_id;
+    echo "<br>Entry successful! Insert ID is: " . $last_id;
+  } else {
+    echo "Error: " . $sql_eventInfo . "<br>" . $conn->error;
   }
+}
 
-  for ($row=0; $row < count($eventStandings) ; $row++) {
-    echo "<p><b>Row number $row</b></p>";
-    echo "<ul>";
-    for ($col=0; $col < count($eventStandings[$row]) ; $col++) {
-      echo "<li>" . $eventStandings[$row][$col] . "</li>";
-    }
-    echo "</ul>";
-  }*/
+enterResult($conn);
 
- ?>
+$conn->close();
+
+
+?>
 
 <html>
 <body>
 
-<form>
-  <button formaction="<?php ?>"
-</form>
+  <form>
+    <button formaction="<?php ?>"
+    </form>
 
 
 
-</body>
-</html>
+  </body>
+  </html>
